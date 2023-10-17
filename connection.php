b4e1a3a7e551523259ff3554dff53bdedc4e4f4f
jeffern malinao <jeffernofficial@gmail.com>
2023-10-18 00:16:23 +0800

    first commit

--- connection.php
+++ connection.php
@@ -1,8 +1,5 @@
 <?php
-
-    $database= new mysqli("localhost","root","","oacms");
-    if ($database->connect_error){
-        die("Connection failed:  ".$database->connect_error);
-    }
-
-?>
+$database = new mysqli("localhost", "root", "@Nkn2mw56a8nju", "oacms");
+if ($database->connect_error) {
+    die("Connection failed:  " . $database->connect_error);
+} ?>
